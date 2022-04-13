0c5d135bdad6b305d2b5f0738334e7e9efdff66e
wyatt.geiger <wyatt.geiger@go.minneapolis.edu>
2022-04-13 02:44:42 -0500

    Added example data, finished lecture video 1

--- video_collection/models.py
+++ video_collection/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+
+class Video(models.Model):
+    name = models.CharField(max_length=200)
+    url = models.CharField(max_length=400)
+    notes = models.TextField(blank=True, null=True) # allows null entries in the database
+
+    def __str__(self):
+        return f'ID: {self.pk}, Name: {self.name}, URL: {self.url}, Notes: {self.notes[:200]}'
+        # truncates notes to first 200 characters
